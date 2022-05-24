13cc2b0c30aed7203ac25403c4df1a7c878c9ebb
unknown <manuelarturosantana@gmail.com>
2022-05-24 15:40:50 -0600

    figured out why it travels left

--- Perfectly_Conductive_Waveguide.m
+++ Perfectly_Conductive_Waveguide.m
@@ -4,25 +4,28 @@
 xVals = linspace(0,pi,100);
 yVals = linspace(0,1,100);
 omegaVals = linspace(-5,5,100);
-times = linspace(0,5,200);
+times = linspace(0,4,200);
 solutions = zeros(length(xVals),length(yVals),length(times));
 
 % Comute sine series of g(x)
 % For simplicity lets assume that g(x) is a sum of sin functions.
 %g = @(x,n) sin(n * x);
 %Function to take the inverse transform of Assume c = 1,sigma=1
-f = @(omega,y,n,t) exp(1i .* sqrt(omega.^2 - n^2) .* y) .* exp(-pi^2 .* omega.^2) .* exp(-1i .* omega .* t);
+f = @(omega,y,n,t) exp(1i .* sqrt(omega.^2 - n^2) .* y) .* exp(-(1/4) .* (omega -2).^2) .* exp(-1i .* omega .* t);
 
 % Numerically Compute inverse Fourier Transform at all points
 for t = 1:length(times)
     v = zeros(length(xVals),length(yVals));
-    for y = 1:length(yVals)
-        for n = 1:1
-            fVals = f(omegaVals,yVals(y),n,times(t));
-            %Assume sigma = 1, and 1/ 2 * pi factor comes in the inverse
-            %transform
-            v(:,y) = v(:,y) + (1 / (2 * sqrt(pi))) * trapz(omegaVals,fVals);
-       end       
+    for x =1:length(xVals)
+        for y = 1:length(yVals)
+            for n = 1:1
+                fVals = f(omegaVals,yVals(y),n,times(t));
+                %Assume sigma = 1, and 1/ 2 * pi factor comes in the inverse
+                %transform
+                v(x,y) = v(x,y) + sin(x) * (1 / (2 * sqrt(pi))) * trapz(omegaVals,fVals);
+    %               v(:,y) = v(:,y) + (1 / (2 * sqrt(pi))) * f(2,yVals(y),n,times(t));
+            end 
+        end
     end
 %     figure(t)
     solutions(:,:,t) = real(v);
@@ -35,7 +38,8 @@ open(vidfile);
 limits = [0,max(solutions(:,:,1),[],'all')];
 for ind = 1:length(times)
    z = solutions(:,:,ind);
-   imagesc(z,limits),colormap(hot) 
+   imagesc(z,limits),colormap(hot)
+   colorbar
    drawnow
    F(ind) = getframe(gcf); 
    writeVideo(vidfile,F(ind));
